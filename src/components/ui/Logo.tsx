4c11ad65602d928bccf2925c977638a5b414fc29
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-04-26 05:11:42 +0100

    refactor: make logo clickable, redirecting to the homepage

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { Activity } from "lucide-react";
 
 function Logo() {
   return (
-    <div className="w-24 h-24 rounded-[28px] bg-linear-to-br from-[#7c3aed] to-[#06b6d4] flex items-center justify-center shadow-2xl">
-      <Activity size={45} className="text-white" />
-    </div>
+    <Link href="/">
+      <div className="w-24 h-24 rounded-[28px] bg-linear-to-br from-[#7c3aed] to-[#06b6d4] flex items-center justify-center shadow-2xl">
+        <Activity size={45} className="text-white" />
+      </div>
+    </Link>
   );
 }
 

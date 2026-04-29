69932aca17ff42ed5156ab74d6609bbfdef55cb6
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-04-29 21:59:37 +0100

    feat:  implement functionality to validate login form

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,15 +1,45 @@
 "use client";
 
 import { useState } from "react";
+import { FormErrors } from "@/types/auth";
 import { Eye, EyeOff } from "lucide-react";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [formErrors, setFormErrors] = useState<FormErrors>({
+    email: "",
+    password: "",
+    containerErr: "",
+  });
+
+  function handleLogin(e: React.SubmitEvent) {
+    e.preventDefault();
+
+    let isValid = true;
+    const errors = { email: "", password: "" };
+
+    if (email === "") {
+      errors.email = "Email is required";
+      isValid = false;
+    } else if (
+      !email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    ) {
+      errors.email = "Please enter a valid email address";
+      isValid = false;
+    }
+
+    if (password === "") {
+      errors.password = "Password is required";
+      isValid = false;
+    }
+
+    setFormErrors(errors);
+  }
 
   return (
-    <form className="lg:w-1/3">
+    <form onSubmit={handleLogin} className="lg:w-1/3">
       <div className="flex flex-col gap-7">
         <div>
           <label
@@ -46,8 +76,15 @@ function LoginForm() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="w-full bg-[#1c1c24] border border-[#2d2d38] rounded-xl pl-10 pr-4 py-3.5 text-white placeholder:text-[#4b4b5a] text-sm focus:outline-none focus:border-[#7c3aed] transition-colors"
+              data-testid="auth-login-email"
             />
           </div>
+
+          {formErrors.email && (
+            <span className="inline-block text-[#ed3548] font-semibold text-sm mt-2">
+              {formErrors.email}
+            </span>
+          )}
         </div>
 
         <div>
@@ -86,16 +123,24 @@ function LoginForm() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full bg-[#1c1c24] border border-[#2d2d38] rounded-xl pl-10 pr-12 py-3.5 text-white placeholder:text-[#4b4b5a] text-sm focus:outline-none focus:border-[#7c3aed] transition-colors"
+              data-testid="auth-login-password"
             />
 
             <button
               type="button"
               onClick={() => setShowPassword(!showPassword)}
               className="absolute right-4 top-1/2 -translate-y-1/2 text-[#6b7280] cursor-pointer"
+              data-testid="auth-login-submit"
             >
               {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
             </button>
           </div>
+
+          {formErrors.password && (
+            <span className="inline-block text-[#ed3548] font-semibold text-sm mt-2">
+              {formErrors.password}
+            </span>
+          )}
         </div>
 
         <button className="w-full py-4 rounded-2xl bg-linear-to-r from-[#7c3aed] to-[#6d28d9] text-white font-semibold text-base shadow-lg active:scale-[0.98] transition-transform cursor-pointer">
